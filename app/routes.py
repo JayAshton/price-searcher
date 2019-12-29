3d20dff0f336071b73e61069bca19109b243e94d
Jay Ashton <j.c.j.ashton@gmail.com>
2019-12-29 23:07:55 +0000

    Added tesco and morrisons scrapers along with results page

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,23 @@
 from flask import render_template
+from flask import request
+
+from app.scrapers.run_tesco import run_tesco
+from app.scrapers.run_morrisons import run_morrisons
+
 from app import app
 
-@app.route('/')
-@app.route('/index')
-
+@app.route('/', methods=["GET", "POST"])
+@app.route('/index', methods=["GET", "POST"])
 def index():
     return render_template('search.html')
+
+@app.route('/search', methods=["GET", "POST"])
+def search():
+    # Get keyword
+    keyword = request.form.get("keyword")
+
+    # Run scraper and get results
+    results = run_morrisons(keyword)
+    print(results)
+
+    return render_template('results.html', results=results)
---
+++ app/scrapers/tesco_groceries/item.py
@@ -0,0 +1,44 @@
+import requests
+from lxml import html
+
+class Item:
+    def visit_item(item_url):
+        page_content = requests.get(item_url)
+        tree = html.fromstring(page_content.content)
+
+        return tree
+
+    def item_validation():
+        # We need to add validation to ensure that we are on the correct page
+        # We might be redirected or be blocked, do not want to scrape random stuff
+        pass
+
+    def get_title(tree):
+        title = tree.xpath('//h1[@class="product-details-tile__title"]/text()')
+        title = ''.join(title)
+
+        return title
+
+    def get_price(tree):
+        price = tree.xpath('//div[@class="price-per-sellable-unit price-per-sellable-unit--price price-per-sellable-unit--price-per-item"]//span[@data-auto="price-value"]/text()')
+        price = ''.join(price)
+
+        return price
+
+    def get_currency(tree):
+        currency = "GBP"
+
+        return currency
+
+    def get_description(tree):
+        description = tree.xpath('//div[@id="product-description"]//li//text()')
+        description = ''.join(description)
+
+        return description
+
+    def get_image(tree):
+        image = tree.xpath('//div[@class="product-image__container"]//img/@src')
+        image = ''.join(image)
+        image = image.split("?")[0]
+
+        return image
