913aba0fed00a4990cb1f3475b2612fec99ef22b
Jay Ashton <j.c.j.ashton@gmail.com>
2020-01-15 22:34:05 +0000

    Removed asyncio

--- app/search/handle_search.py
+++ app/search/handle_search.py
@@ -1,21 +1,13 @@
 from flask import render_template
 from app import app
 
-import asyncio
-
 from app.scrapers.run_tesco import run_tesco
 from app.scrapers.run_morrisons import run_morrisons
 
 class Search():
-    async def handle_search(keyword):
-        #tesco_results = run_tesco(keyword)
-        #morrisons_results = run_morrisons(keyword)
-
-        tesco_scraper = asyncio.create_task(run_tesco(keyword))
-        morrisons_scraper = asyncio.create_task(run_morrisons(keyword))
-
-        tesco_results = await tesco_scraper
-        morrisons_results = await morrisons_scraper
+    def handle_search(keyword):
+        tesco_results = run_tesco(keyword)
+        morrisons_results = run_morrisons(keyword)
 
         return render_template('results.html',
         tesco_results=tesco_results,
